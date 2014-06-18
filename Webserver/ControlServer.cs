15f5ae4a28166f09c308e0aa5e22dbb7bd8c9640
Stefan van der Pas <stefan10pas@hotmail.com>
2014-06-18 15:49:02 +0200

    deleted redirect ( = handleget)

--- Webserver/ControlServer.cs
+++ Webserver/ControlServer.cs
@@ -147,7 +147,7 @@ namespace Webserver
                 sRequest += "/";
 
             // Get the directory
-            String sRequestDirectoryName = sRequest.Substring(sRequest.IndexOf("/"), sRequest.LastIndexOf("/") - 3);
+            String sRequestDirectoryName = sRequest.Substring(sRequest.IndexOf("/"), sRequest.LastIndexOf("/") - 4);
             // If directory is root then give empty string
             sRequestDirectoryName = (sRequestDirectoryName.Equals("/")) ? "" : sRequestDirectoryName;
 
@@ -238,9 +238,8 @@ namespace Webserver
             {
                 case "info": loginMethod(sPostMethod, dPostData, sRequestedFile, sRequest, sHttpVersion, sslStream); break; // from login to info
                 case "login": loginMethod(sPostMethod, dPostData, sRequestedFile, sRequest, sHttpVersion, sslStream); break;
-                case "createuser": redirect(sRequestedFile, sRequest, sHttpVersion, sslStream); break; // from info to add new user
+                case "createuser": handleGetRequest(sRequest, sHttpVersion, sslStream); break; // from info to add new user
                 case "new": createMethod(sPostMethod, dPostData, sRequestedFile, sRequest, sHttpVersion, sslStream); break;
-                
                 default: SendErrorPage(404, sHttpVersion, sslStream); break;
             }
         }
@@ -251,6 +250,8 @@ namespace Webserver
             String password = dPostData.ElementAt(1).Value;
             String right = dPostData.ElementAt(2).Value;
             _mySqlModule.CreateUser(username, password, right);
+
+            handleGetRequest(sRequest, sHttpVersion, sslStream);
         }
 
         private void loginMethod(String sPostMethod, Dictionary<String, String> dPostData, String sRequestedFile, String sRequest, String sHttpVersion, SslStream sslStream)
@@ -261,54 +262,12 @@ namespace Webserver
             Dictionary<bool, string> loginResult = _mySqlModule.CheckUser(username, password);
             if(loginResult.ElementAt(0).Key == true)
             {
-                redirect(sRequestedFile, sRequest, sHttpVersion, sslStream);
+                handleGetRequest(sRequest, sHttpVersion, sslStream);
             }
             else
             {
                 //SendErrorPage(400, sHttpVersion, sslStream);
             }
-        }
-
-        private void redirect(String sRequestedFile, String sRequest, String sHttpVersion, SslStream sslStream)
-        {
-            // send everything to browsers if login is true
-            // Get the directory
-            String sRequestDirectoryName = sRequest.Substring(sRequest.IndexOf("/"), sRequest.LastIndexOf("/") - 4);
-            // If directory is root then give empty string
-            sRequestDirectoryName = (sRequestDirectoryName.Equals("/")) ? "" : sRequestDirectoryName;
-
-            // Add 'control' to directory name, as it's the control server
-            String sDirectoryName = _fileModule.CombinePaths("control", sRequestDirectoryName);
-
-            // Check if localPath exists
-            String sLocalPath = _fileModule.GetLocalPath(sDirectoryName);
-            if (String.IsNullOrEmpty(sLocalPath))
-            {
-                SendErrorPage(404, sHttpVersion, sslStream);
-                return;
-            }
-
-            // Check if file is given, get default file if not given
-            //String sRequestedFile = sRequest.Substring(sRequest.LastIndexOf("/") + 1);
-            if (string.IsNullOrEmpty(sRequestedFile))
-            {
-                sRequestedFile = _fileModule.GetControlDefaultPage(sLocalPath);
-            }
-
-            // Check file mimetype
-            String mimeType = _fileModule.GetMimeType(sRequestedFile);
-            if (String.IsNullOrEmpty(mimeType))
-            {
-                SendErrorPage(404, sHttpVersion, sslStream);
-                return;
-            }
-
-            // File to bytes
-            String sPhysicalFilePath = Path.Combine(sLocalPath, sRequestedFile);
-            byte[] bFileBytes = _fileModule.FileToBytes(sPhysicalFilePath);
-
-            SendHeader(sHttpVersion, mimeType, bFileBytes.Length, "200 OK", sslStream);
-            SendToBrowser(bFileBytes, sslStream);
         }
     }
 }
